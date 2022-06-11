65966de70601508652b6b047439979b272925ef6
Wilfried Haderer <w.haderer@gmx.net>
2022-06-11 19:42:33 +0200

    new pipelineJob 'theme-park-job-docker'

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -13,3 +13,19 @@ pipeline {
         }
     }
 }
+
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
